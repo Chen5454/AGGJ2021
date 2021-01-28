b2a27beeeb0d90434e29ae2c0b71076af094747f
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-01-28 23:08:56 +0200

    Basic Movement
    Basic Movement

---
+++ AGGJ2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = input.normalized * speed;
+        transform.position += velocity * Time.deltaTime;
+    }
+}
