7a029a707d4711fceace682d2defa8cfed3ccffb
Chen5454 <74614370+Chen5454@users.noreply.github.com>
2021-01-29 18:20:23 +0200

    Add Object Push.
    Still need to work on the space between them+Rotation Player to where he's looking

--- AGGJ2021/Assets/Scripts/PlayerMovement.cs
+++ AGGJ2021/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
-
+    public GameObject pushedItems;
+    bool heldByPlayer = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,5 +20,41 @@ public class PlayerMovement : MonoBehaviour
         var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 velocity = input.normalized * speed;
         rb.velocity = velocity; // Using RigidBody velocity instead of generic Transform in order to prevent jitter when interacting with physics objects
+        rb.rotation = Quaternion.LookRotation(input);
+
+
+        if (heldByPlayer)
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                pushedItems.transform.position = gameObject.transform.position;
+
+            }
+          
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag == "PChair")
+        {
+
+            heldByPlayer = true;
+
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "PChair")
+        {
+
+            heldByPlayer = false;
+
+        }
     }
 }
