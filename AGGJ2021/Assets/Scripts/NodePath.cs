4c764a9b42cb26d6650eb7334aa7efb9556df910
Ursula <edenben13@hotmail.com>
2021-01-29 20:45:36 +0200

    COS agent movement

---
+++ AGGJ2021/Assets/Scripts/NodePath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodePath : MonoBehaviour
+{
+    public GameObject[] Nodes;
+
+    public int currNodeNum;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 getCurrNode()
+    {
+        return Nodes[currNodeNum].transform.position;
+    
+    }
+
+    public void MoveToNextNode()
+    {
+        currNodeNum++;
+
+        if (Nodes.Length <= currNodeNum)
+        {
+            currNodeNum = 0;
+        }
+    }
+
+}
